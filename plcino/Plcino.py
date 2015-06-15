4f43667614047f2402a7fbb3b6f89035b08b3de2
meji <margalmagister@gmail.com>
2015-06-15 10:17:36 +0200

    Revision 1

--- plcino/Plcino.py
+++ plcino/Plcino.py
@@ -169,14 +169,8 @@ def contactNeg(input, var, imtc):
             return 0
 
 ############################# LOGIC ###################################
-def nott (input):
+def nott(input):
     if (input == 0):
-        return 1
-    else:
-        return 0
-
-def orr (input1, input2):
-    if (input1 == 1)|(input2 == 1):
         return 1
     else:
         return 0
@@ -247,8 +241,7 @@ def counter(cu, cd, r, pv, var):
             countAux[var] = countAux[var] +1
 
         if (cd == 1)&(countPrevCD[var]!= 1):
-            if (countAux[var] > 0):
-                countAux[var] = countAux[var] - 1
+            countAux[var] = countAux[var] - 1
 
         if (countAux[var] >= pv):
             counts[var] = 1
@@ -299,6 +292,9 @@ while(1): # RUN STATE
 
     ########### Blinked led example ###################
     #
+    # In this example, the blinked led is in output[0] = pin 2 in arduino (0 and 1 are tx and rx)
+
+    #
     #   m0      ___t0__
     # --| |-----|   tON|
     #           |      |
@@ -321,19 +317,24 @@ while(1): # RUN STATE
     ####################################################
 
     # Its the same as --ton(contact(1,marks[0]),1,0)-- but if the contact input is 1 always, you can skip it
-    ton(marks[0],1,0)
+#    ton(marks[0],1,0)
 
     # To do the fork, there are 2 options. This is the first:
-    ton(timers[0],1,1)
-    coil(timers[0],0,"q")
+#    ton(timers[0],1,1) # Set the timer with: input = value of timer 0, pt = 1 second, number of this thimer = 1
+#    coil(timers[0],0,"q") # Set the coil with: input = value of timer 0, number of output/mark = 0, type = output
 
     # And this is the second (this is better if you have more logic before the fork):
-    # aux = timers[0]
-    # ton(aux,1,1)
-    # coil(aux,0,"q")
+    #aux = timers[0]
+    #ton(aux,1,1)
+    #coil(aux,0,"q")
 
-    coilSet(1,0,"m")
-    coilReset(timers[1],0,"m")
+#    coilSet(1,0,"m") # Set the mark 0 allwais (input is 1)
+#    coilReset(timers[1],0,"m") # Reset the mark 0 when timer1 = 1
+
+    #####################################################
+
+    print contact((1 or 0),1)
+    print"\teee"
 
 
 # WRITE THE OUTPUTS ///////////////////////////////////////////////////////////
